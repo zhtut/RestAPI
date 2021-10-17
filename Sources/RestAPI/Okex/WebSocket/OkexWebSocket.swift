f54d9e7b3db50fe9f37ac0da1857956fb8a22ce9
周途广 <zhtg@icloud.com>
2021-10-17 16:14:18 +0800

    优化日志

--- Sources/RestAPI/Okex/WebSocket/OkexWebSocket.swift
+++ Sources/RestAPI/Okex/WebSocket/OkexWebSocket.swift
@@ -84,7 +84,7 @@ open class OkexWebSocket: SCWebSocket {
     open override func webSocketDidReceive(message: [String : Any]) {
         super.webSocketDidReceive(message: message)
         let str = message.jsonStr
-        log("WebsocketDidReceive:\(str)")
+        log("WebsocketDidReceive:\(str ?? "")")
         if let event = message["event"] as? String,
            let arg = message["arg"] as? [String: Any],
            let channel = arg["channel"] as? String,
