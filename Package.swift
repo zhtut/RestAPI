6b0c58838169a835a0b4d48273e136d96b21887e
周途广 <zhtg@icloud.com>
2022-01-08 12:57:54 +0800

    更新地址

--- Package.swift
+++ Package.swift
@@ -16,9 +16,9 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://gitee.com/ztgtut/SSNetwork.git", branch: "master"),
-        .package(url: "https://gitee.com/ztgtut/SSCommon.git", branch: "master"),
-        .package(url: "https://gitee.com/ztgtut/SSWebsocket.git", branch: "master"),
+        .package(url: "https://github.com/zhtut/SSNetwork.git", branch: "master"),
+        .package(url: "https://github.com/zhtut/SSCommon.git", branch: "master"),
+        .package(url: "https://github.com/zhtut/SSWebsocket.git", branch: "master"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
