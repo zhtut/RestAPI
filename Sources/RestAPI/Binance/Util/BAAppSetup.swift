54dbce41c98c2379ee9f5f858fe36fc5c3828160
周途广 <zhtg@icloud.com>
2022-08-28 02:03:56 +0800

    优化初始化方法，防止进两次

--- Sources/RestAPI/Binance/Util/BAAppSetup.swift
+++ Sources/RestAPI/Binance/Util/BAAppSetup.swift
@@ -16,9 +16,8 @@ open class BAAppSetup {
     open var instId = "ETHBUSD"
 
     open var instrument: Instrument!
-    open var completion: SucceedHandler?
     
-    open func setup(instId: String, completion: @escaping SucceedHandler) {
+    open func setup(instId: String) async throws {
         log("setup App方法，开始app")
         
         sendPushNotication("开始记录日志")
@@ -29,63 +28,58 @@ open class BAAppSetup {
         let _ = BAPosManager.shared
         
         self.instId = instId
-        self.completion = completion
         
         let bookTickerManger = BABookTickerManger.shared
         bookTickerManger.instId = instId
         bookTickerManger.subcribeDepth()
         
         log("开始请求产品信息")
-        requestInstrument { succ, errMsg in
-            if succ {
-                log("请求产品信息成功，初始化完成")
-                guard let _ = self.instrument else {
-                    log("请求产品信息失败：\(errMsg ?? "")，程序退出")
-                    exit(1)
-                }
-                let websocket = BAUserWebSocket.shared
+        do {
+            try await requestInstrument()
+            log("请求产品信息成功，初始化完成")
+            guard let _ = self.instrument else {
+                throw CommonError(errMsg: "没有找到instrument")
+            }
+            let websocket = BAUserWebSocket.shared
+            return await withCheckedContinuation { continuation in
                 websocket.didReadyBlock = {
                     BAPosManager.shared.configLever()
-                    self.completion?(true, nil)
+                    continuation.resume()
                 }
-            } else {
-                log("请求产品信息失败：\(errMsg ?? "")，程序退出")
-                exit(1)
             }
+        } catch {
+            log("请求产品信息失败：\(error)，程序退出")
         }
     }
     
-    open func requestInstrument(completion: @escaping SucceedHandler) {
+    open func requestInstrument() async throws {
         let path = "GET /fapi/v1/exchangeInfo"
-        BARestAPI.sendRequestWith(path: path, dataKey: "symbols") { response in
-            if response.responseSucceed {
-                guard let arr = response.data as? [[String: Any]] else {
-                    completion(false, "BAInstrument返回格式有问题")
+        let response = await BARestAPI.sendRequestWith(path: path, dataKey: "symbols")
+        if response.responseSucceed {
+            guard let arr = response.data as? [[String: Any]] else {
+                throw CommonError(errMsg: "BAInstrument返回格式有问题")
+            }
+            for dic in arr {
+                let ins = BAInstrument.modelWith(dic: dic)
+                if ins.symbol == self.instId {
+                    var instrument = Instrument()
+                    instrument.instId = ins.symbol ?? "" /// : String?
+                    /// 交易货币币种，如 BTC-USDT 中BTC ，仅适用于币币
+                    instrument.baseCcy = ins.baseAsset ?? "" ///: String?
+                    /// 计价货币币种，如 BTC-USDT 中 USDT ，仅适用于币币
+                    instrument.quoteCcy = ins.quoteAsset ?? "" /// : String?
+                    /// 下单价格精度，如 0.0001
+                    instrument.tickSz = ins.tickSz ?? "" ///: String?
+                    /// 下单数量精度，如 1：BTC-USDT-200925 0.001：BTC-USDT
+                    instrument.lotSz = ins.lotSz ?? "" /// : String?
+                    /// 下单最小值
+                    instrument.minSz = ins.minSz ?? ""
+                    self.instrument = instrument
                     return
                 }
-                for dic in arr {
-                    let ins = BAInstrument.modelWith(dic: dic)
-                    if ins.symbol == self.instId {
-                        var instrument = Instrument()
-                        instrument.instId = ins.symbol ?? "" /// : String?
-                        /// 交易货币币种，如 BTC-USDT 中BTC ，仅适用于币币
-                        instrument.baseCcy = ins.baseAsset ?? "" ///: String?
-                        /// 计价货币币种，如 BTC-USDT 中 USDT ，仅适用于币币
-                        instrument.quoteCcy = ins.quoteAsset ?? "" /// : String?
-                        /// 下单价格精度，如 0.0001
-                        instrument.tickSz = ins.tickSz ?? "" ///: String?
-                        /// 下单数量精度，如 1：BTC-USDT-200925 0.001：BTC-USDT
-                        instrument.lotSz = ins.lotSz ?? "" /// : String?
-                        /// 下单最小值
-                        instrument.minSz = ins.minSz ?? ""
-                        self.instrument = instrument
-                        completion(true, nil)
-                        return
-                    }
-                }
             }
-            
-            completion(false, response.errMsg)
         }
+        
+        throw CommonError(errMsg: response.errMsg ?? "")
     }
 }
