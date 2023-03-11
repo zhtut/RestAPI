16c4e5d7e64cc194214d351242d672b615e1c6ac
周途广 <zhtg@icloud.com>
2023-03-11 23:59:07 +0800

    更新

--- Package.swift
+++ Package.swift
@@ -19,16 +19,16 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        //        .package(url: "https://github.com/zhtut/SSNetwork.git", branch: "main"),
-        .package(name: "SSNetwork", path: "\(toolsPath)/SSNetwork"),
-        //        .package(url: "https://github.com/zhtut/SSWebsocket.git", branch: "main"),
-        .package(name: "SSWebsocket", path: "\(toolsPath)/SSWebsocket"),
-        //        .package(url: "https://github.com/zhtut/SSCommon.git", branch: "main"),
-        .package(name: "SSCommon", path: "\(toolsPath)/SSCommon"),
-        //        .package(url: "https://github.com/zhtut/SSLog.git", branch: "main"),
-        .package(name: "SSLog", path: "\(toolsPath)/SSLog"),
-        //        .package(url: "https://github.com/zhtut/SSEncrypt.git", branch: "main"),
-        .package(name: "SSEncrypt", path: "\(toolsPath)/SSEncrypt"),
+        .package(url: "https://github.com/zhtut/SSNetwork.git", branch: "main"),
+        //        .package(name: "SSNetwork", path: "\(toolsPath)/SSNetwork"),
+        .package(url: "https://github.com/zhtut/SSWebsocket.git", branch: "main"),
+        //        .package(name: "SSWebsocket", path: "\(toolsPath)/SSWebsocket"),
+        .package(url: "https://github.com/zhtut/SSCommon.git", branch: "main"),
+        //        .package(name: "SSCommon", path: "\(toolsPath)/SSCommon"),
+        .package(url: "https://github.com/zhtut/SSLog.git", branch: "main"),
+        //        .package(name: "SSLog", path: "\(toolsPath)/SSLog"),
+        .package(url: "https://github.com/zhtut/SSEncrypt.git", branch: "main"),
+        //        .package(name: "SSEncrypt", path: "\(toolsPath)/SSEncrypt"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
